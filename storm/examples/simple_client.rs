1d541f22e29061e075dbdc15e57df3801d0e37e6
Yang Keao <keao.yang@yahoo.com>
2019-03-10 19:43:32 +0800

    Add Sync function

--- storm/examples/simple_client.rs
+++ storm/examples/simple_client.rs
@@ -4,6 +4,7 @@ extern crate log;
 extern crate env_logger;
 
 use log::Level;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use storm::wayland;
 use storm::wayland::*;
@@ -14,6 +15,12 @@ fn main() {
     let mut client = storm::client::Client::connect(None);
     println!("Connected to display");
 
+    let mut wl_compositor_id = Arc::new(Mutex::new(0));
+    let mut c_wl_compositor_id = wl_compositor_id.clone();
+
+    let mut wl_shell_id = Arc::new(Mutex::new(0));
+    let mut c_wl_shell_id = wl_shell_id.clone();
+
     let mut c_client = client.clone();
     client.add_event_listener(Box::new(move |event| match event {
         wayland::Event::WlRegistryEvent(reg_ev) => match reg_ev {
@@ -22,48 +29,29 @@ fn main() {
                     "WlRegistryGlobalEvent: Name: {}, Interface: {}",
                     gl_ev.name, gl_ev.interface
                 );
-
                 if gl_ev.interface == "wl_compositor" {
-                    c_client.bind_obj::<WlCompositor>(3);
+                    let mut obj_id = c_wl_compositor_id.lock().unwrap();
+                    *obj_id = c_client.new_obj::<WlCompositor>();
                     c_client
                         .get_obj(gl_ev.sender_id)
                         .unwrap()
                         .try_get_wl_registry()
                         .unwrap()
-                        .bind(gl_ev.name, String::from("wl_compositor"), gl_ev.version, 3);
+                        .bind(
+                            gl_ev.name,
+                            String::from("wl_compositor"),
+                            gl_ev.version,
+                            *obj_id,
+                        );
                 } else if gl_ev.interface == "wl_shell" {
-                    c_client.bind_obj::<WlShell>(4);
+                    let mut obj_id = c_wl_shell_id.lock().unwrap();
+                    *obj_id = c_client.new_obj::<WlShell>();
                     c_client
                         .get_obj(gl_ev.sender_id)
                         .unwrap()
                         .try_get_wl_registry()
                         .unwrap()
-                        .bind(gl_ev.name, String::from("wl_shell"), gl_ev.version, 4);
-                }
-
-                if c_client.get_obj(4).is_some() && c_client.get_obj(3).is_some() {
-                    c_client.bind_obj::<WlSurface>(5);
-                    c_client
-                        .get_obj(3)
-                        .unwrap()
-                        .try_get_wl_compositor()
-                        .unwrap()
-                        .create_surface(5);
-
-                    c_client.bind_obj::<WlShellSurface>(6);
-                    c_client
-                        .get_obj(4)
-                        .unwrap()
-                        .try_get_wl_shell()
-                        .unwrap()
-                        .get_shell_surface(6, 5);
-
-                    c_client
-                        .get_obj(6)
-                        .unwrap()
-                        .try_get_wl_shell_surface()
-                        .unwrap()
-                        .set_toplevel();
+                        .bind(gl_ev.name, String::from("wl_shell"), gl_ev.version, *obj_id);
                 }
             }
             _ => {}
@@ -74,8 +62,35 @@ fn main() {
     client.bind_obj::<WlRegistry>(2);
     client.get_display().get_registry(2);
     println!("Get Registry at id 2");
+    client.sync();
+    println!("Wayland Sync");
 
-    loop {}
+    let mut wl_compositor_id = *(wl_compositor_id.lock().unwrap());
+    let mut wl_shell_id = *(wl_shell_id.lock().unwrap());
+
+    let mut wl_surface_id = client.new_obj::<WlSurface>();
+    client
+        .get_obj(wl_compositor_id)
+        .unwrap()
+        .try_get_wl_compositor()
+        .unwrap()
+        .create_surface(wl_surface_id);
+
+    let mut wl_shell_surface_id = client.new_obj::<WlShellSurface>();
+    client
+        .get_obj(wl_shell_id)
+        .unwrap()
+        .try_get_wl_shell()
+        .unwrap()
+        .get_shell_surface(wl_shell_surface_id, wl_surface_id);
+
+    client
+        .get_obj(wl_shell_surface_id)
+        .unwrap()
+        .try_get_wl_shell_surface()
+        .unwrap()
+        .set_toplevel();
+
     client.disconnect();
 
     println!("Disconnected from display");
--- storm/src/client.rs
+++ storm/src/client.rs
@@ -1,15 +1,17 @@
 use super::socket::WaylandSocket;
 use super::wayland;
 use super::wayland::{WlDisplay, WlObject, WlRawObject};
+use crate::wayland::{IWlDisplay, WlCallback};
 use std::collections::HashMap;
 use std::sync::Mutex;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Condvar, RwLock};
 use std::thread;
 
 #[derive(Clone)]
 pub struct Client {
     socket: Arc<WaylandSocket>,
     pub obj_map: Arc<Mutex<HashMap<u32, Arc<WlObject>>>>,
+    pub max_id: Arc<Mutex<u32>>,
     pub event_listeners: Arc<RwLock<Vec<Box<Fn(&wayland::Event) + Send + Sync>>>>,
 }
 
@@ -20,6 +22,7 @@ impl Client {
         let client = Client {
             socket,
             obj_map: Arc::new(Mutex::new(HashMap::new())),
+            max_id: Arc::new(Mutex::new(0)),
             event_listeners: Arc::new(RwLock::new(Vec::new())),
         };
         client.bind_obj::<WlDisplay>(1);
@@ -48,13 +51,63 @@ impl Client {
         self.get_obj(1).unwrap().try_get_wl_display().unwrap()
     }
 
+    pub fn sync(&self) {
+        let callback_id = self.new_obj::<WlCallback>();
+        let this = self.clone();
+
+        let done_pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let c_done_pair = done_pair.clone();
+        self.add_event_listener(Box::new(move |ev| match ev {
+            wayland::Event::WlCallbackEvent(callback_ev) => match callback_ev {
+                wayland::WlCallbackEvent::WlCallbackDoneEvent(done) => {
+                    if done.sender_id == callback_id {
+                        info!("Callback id {} Done", callback_id);
+                        this.delete_obj(callback_id);
+
+                        let &(ref done, ref cond_var) = &*c_done_pair;
+                        *(done.lock().unwrap()) = true;
+                        cond_var.notify_all();
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }));
+        self.get_display().sync(callback_id);
+
+        let &(ref done, ref cond_var) = &*done_pair;
+        let mut done = done.lock().unwrap();
+        while !*done {
+            done = cond_var.wait(done).unwrap();
+        }
+    }
+
     pub fn get_obj(&self, obj_id: u32) -> Option<Arc<WlObject>> {
         Some(self.obj_map.lock().unwrap().get(&obj_id)?.clone())
+    }
+
+    pub fn new_obj<T: WlRawObject>(&self) -> u32 {
+        let mut hash_map = self.obj_map.lock().unwrap();
+        let mut now_max = self.max_id.lock().unwrap();
+
+        let new_id = now_max.clone() + 1;
+        let wl_obj = Arc::new(T::new(new_id, self.socket.clone()).to_enum());
+        hash_map.insert(new_id, wl_obj.clone());
+        *(now_max) += 1;
+
+        return new_id;
+    }
+
+    pub fn delete_obj(&self, obj_id: u32) {
+        self.obj_map.lock().unwrap().remove(&obj_id);
     }
 
     pub fn bind_obj<T: WlRawObject>(&self, obj_id: u32) {
         let wl_obj = Arc::new(T::new(obj_id, self.socket.clone()).to_enum());
         self.obj_map.lock().unwrap().insert(obj_id, wl_obj.clone());
+
+        let now_max = self.max_id.lock().unwrap().clone();
+        *(self.max_id.lock().unwrap()) = std::cmp::max(now_max, obj_id);
     }
 
     pub fn add_event_listener(&self, event_handler: Box<Fn(&wayland::Event) + Send + Sync>) {
